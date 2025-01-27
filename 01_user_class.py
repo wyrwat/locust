37948a11ad0c2d3293a7df80d03d461d8a1d588a
wyrwat <wyrwa.t@gmail.com>
2025-01-27 18:47:58 +0100

    create two test simple tests with weight and wait_time

---
+++ 01_user_class.py
@@ -0,0 +1,26 @@
+from locust import User, task, constant
+
+
+class MyFirstTest(User):
+    weight = 2
+    wait_time = constant(1)
+    @task
+    def launch(self):
+        print('Launching the URL')
+
+    @task
+    def search(self):
+        print('Searching')
+
+
+class MySecondTask(User):
+    weight = 2
+    wait_time = constant(1)
+
+    @task
+    def launch2(self):
+        print('Second Test')
+
+    @task
+    def search2(self):
+        print('Second searching')
